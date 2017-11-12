4e846d3ecf8a8eac5ca3cc90f80ed9b747fdfb27
Johnny Zavala <jzavalafgz@gmail.com>
2017-11-12 01:07:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const switchOnCharmFromTip = (tip) => {
   if (tip === 'generous') {
     return 'Thank you so much.'
   }
-  else if (tip !== 'generous') {
+  else if (tip === 'not as generous') {
     return 'Thank you.'
   }
   else if (tip === 'Thank you.') {
