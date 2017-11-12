f83ca143fcb11927d2869a71a09e669d02067290
Johnny Zavala <jzavalafgz@gmail.com>
2017-11-12 00:50:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function scuberGreetingForFeet(height) {
   if (height <= 400) {
     return 'This one is on me!';
   }
+  else if (height > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
